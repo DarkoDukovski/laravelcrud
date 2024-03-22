e0ecced4ee819e390c1d79597ffc10c5bed2fce1
DarkoDukovski <darkodukovski1@gmail.com>
2024-03-22 16:47:34 +0100

    Added function to fill the universities in the database

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,79 +4,57 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Pagination\Paginator;
+use App\Models\Universities;
 
 class ApiController extends Controller
 {
     public function index(Request $request)
     {
-        // Fetch initial universities data
+        
         $initialResponse = Http::get('http://universities.hipolabs.com/search?country=United+States');
         $universities = $initialResponse->json();
 
-        // Selecting required fields
-        $universities = array_map(function($university) {
-            return [
-                'Name' => $university['name'],
-                'Country' => $university['country'],
-                'Code' => $university['alpha_two_code'],
-                'Domain' => $university['domains'][0] ?? '',
-                'Web Page' => $university['web_pages'][0] ?? ''
-            ];
-        }, $universities);
+        foreach ($universities as $university) {
+            $universityName = $university['name'];
+            
+            
+            if (!Universities::where('name', $universityName)->exists()) {
+                $newUniversity = new Universities();
+                $newUniversity->name = $universityName;
+                $newUniversity->save();
+            }
+        }
 
-        // Paginate the data
-        $universities = $this->paginateUniversities($universities, 3000);
-
-        // Set $showGetApiButton to true to show the "Get API" button
+        
         $showGetApiButton = true;
 
-        return view('api.universities', compact('universities', 'showGetApiButton'));
+        
+        $universitiesNames = Universities::pluck('name');
+
+        return view('api.universities', compact('universitiesNames', 'showGetApiButton'));
     }
 
     public function fetchUniversities(Request $request)
     {
-        // Fetch universities data
-        {
-    // Fetch universities data
-    $page = $request->input('page', 1);
-    $response = Http::get('http://universities.hipolabs.com/search?country=United+States&page=' . $page);
-    $universities = $response->json();
+        
+        $page = $request->input('page', 1);
+        $response = Http::get('http://universities.hipolabs.com/search?country=United+States&page=' . $page);
+        $universities = $response->json();
 
-    // Return JSON response
-    return response()->json($universities);
-}
-
-        // Selecting required fields
-        $universities = array_map(function($university) {
-            return [
-                'Name' => $university['name'],
-                'Country' => $university['country'],
-                'Code' => $university['alpha_two_code'],
-                'Domain' => $university['domains'][0] ?? '',
-                'Web Page' => $university['web_pages'][0] ?? ''
-            ];
-        }, $universities);
-
-        // Paginate the data
-        $universities = $this->paginateUniversities($universities, 3000);
-
-        // Set $showGetApiButton to false to hide the "Get API" button
-        $showGetApiButton = false;
-
-        return view('api.universities', compact('universities', 'showGetApiButton'));
+        
+        return response()->json($universities);
     }
 
     public function search(Request $request)
     {
-        // Get search query from the request
+        
         $search = $request->input('search');
 
-        // Fetch filtered universities data based on the search query
+        
         $response = Http::get('http://universities.hipolabs.com/search?country=United+States', ['name' => $search]);
         $universities = $response->json();
 
-        // Selecting required fields
+        
         $universities = array_map(function($university) {
             return [
                 'Name' => $university['name'],
@@ -87,16 +65,16 @@ class ApiController extends Controller
             ];
         }, $universities);
 
-        // Paginate the data
+
         $universities = $this->paginateUniversities($universities, 3000);
 
-        // Set $showGetApiButton to false to hide the "Get API" button
+        
         $showGetApiButton = false;
 
         return view('api.universities', compact('universities', 'showGetApiButton'));
     }
 
-    // Helper function to paginate universities
+    
     private function paginateUniversities($universities, $perPage)
     {
         $currentPage = Paginator::resolveCurrentPage('page');
